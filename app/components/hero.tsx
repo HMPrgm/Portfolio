76e989be489f98dff1416047cd9ac3a0c4f68ba9
MSI Laptop <henrymunprof@gmail.com>
2024-06-20 12:34:28 -0400

    started on hero section

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,7 +1,8 @@
 export default function Hero() {
   return (
-    <div>
-      
+    <div className="max-w-[1640px] mx-auto">
+      <h2 className="text-5xl">Hello World</h2>
+      <h3></h3>
     </div>
   )
 }
