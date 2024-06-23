242091cf59a1a9a21f8cf33c10830970244b8486
HMPrgm - PC <henrymprof@gmail.com>
2024-06-22 22:38:54 -0400

    Updated About Section

--- app/components/home/About.tsx
+++ app/components/home/About.tsx
@@ -1,7 +1,10 @@
+import About_section from "./About_section";
+
 export default function About() {
   return (
     <section className="bw-text flex flex-col justify-center text-center py-10">
-        <h2 className="font-playfairDisplay text-5xl">Who Am I?</h2>
+        <h2 className="font-playfairDisplay text-5xl pb-10">Who Am I?</h2>
+        <About_section></About_section>
     </section>
   )
 }
--- app/components/home/About_section.tsx
+++ app/components/home/About_section.tsx
@@ -1,5 +1,17 @@
 export default function About_section() {
+
+
+
   return (
-    <div>About_section</div>
+    <div className="py-10 container mx-auto lg:w-[70rem] sm:w-[50rem] flex flex-col gap-16">
+      <div className="grid grid-cols-2 gap-10">
+        <div></div>
+        <div className="flex flex-col gap-2 text-left">
+          <h3 className="font-playfairDisplay text-3xl">I’m studying Computer Science at the University of Virginia</h3>
+          <p>Studying CS through UVA Engineering along with a Math major. Aside from those, my other interests include: history, language learning (German currently), reading, and exercising!</p>
+        </div>
+      </div>
+      <hr className="h-0.25 bg-Cwhite"/>
+    </div>
   )
 }
