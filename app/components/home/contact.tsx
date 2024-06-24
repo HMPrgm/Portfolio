8850d7b3feef5bf6f4f9741346ee59929ab49689
MSI Laptop <henrymunprof@gmail.com>
2024-06-24 14:01:35 -0400

    Contact v1

--- app/components/home/contact.tsx
+++ app/components/home/contact.tsx
@@ -10,24 +10,28 @@ export default function Contact() {
     }>({ name: "", email: "", message: "" });
 
     return (
-        <div className="bg-Cwhite py-10">
+        <div className="bg-Cwhite py-10 text-Cblack">
             <div className="container mx-auto items-center text-center ">
-                <h2 className="font-playfairDisplay text-5xl  text-Cblack">Let's Talk!</h2>
-                <form action="/email" method="POST">
-                    <div>
-                        <label htmlFor="name"> Name
-                            <input type="text" name="name" id="name" />
-                        </label>
-                        <label htmlFor="name"> Email
-                            <input type="email" name="email" id="email" />
-                        </label>
-                    </div>
-                    <div>
-                        <label htmlFor="message">Message
-                            <textarea name="message" id="message" cols={5}></textarea>
-                        </label>
-                    </div>
-                </form>
+                <div className="container items-center mx-auto max-w-96 [&>*]:py-4">
+                    <h2 className="font-playfairDisplay text-5xl  text-Cblack">Let's Talk!</h2>
+                    <p className="text-xl">Have a question or just want to talk? Feel free to shoot me a message!</p>
+                </div>
+                <div className="my-10 max-w-[60em] container items-center mx-auto">
+                    <form action="/email" method="POST"className="grid grid-cols-2 gap-4 [&>*]:flex [&>*]:flex-col text-left text-xl">
+                            <div className="">
+                                <label htmlFor="name" className="">Name</label>
+                                <input type="name" name="name" id="name" className="focus:outline-none bg-Cwhite border-b-Cblack border-b-2 py-1" placeholder="Enter your name"></input>
+                            </div>
+                            <div>
+                                <label htmlFor="email" >Email</label>
+                                <input type="email" name="email" id="email" placeholder="Enter your email address" className="focus:outline-none bg-Cwhite border-b-Cblack border-b-2 py-1"></input>
+                            </div>
+                            <div className="col-span-2">
+                                <label htmlFor="message" className="">Message</label>
+                                <textarea name="message" id="message" className="focus:outline-none h-18 overflow-auto resize-none bg-Cwhite border-b-Cblack border-b-2 py-1.5" placeholder="Hi Henry, I was just curious about your X project and was wondering if you could tell me more about how you implemented..."></textarea>
+                            </div>
+                    </form>
+                </div>
             </div>
         </div>
     )
