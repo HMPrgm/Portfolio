5a6f4269ef647c12328e9f8b694172ff88265454
MSI Laptop <henrymunprof@gmail.com>
2024-06-24 17:18:09 -0400

    typewriter effect

--- app/components/home/hero.tsx
+++ app/components/home/hero.tsx
@@ -1,11 +1,19 @@
+'use client'
+import {ReactTyped} from 'react-typed'; 
+import Dots from '../partials/dots';
+
 export default function Hero() {
+
+
   return (
     <section id="home" className="items-center text-center bw-text font-playfairDisplay flex flex-col gap-12 min-h-screen justify-center ">
+      <Dots></Dots>
       <h2 className="text-8xl font-bold typewriter">
-        Hi, I'm Henry
+      <ReactTyped strings={['Hi, I\'m Henry']} typeSpeed={100} cursorChar=''/>
       </h2>
-      <h3 className="text-4xl">
-        A CS student at UVA
+      <h3 className="text-4xl min-h-10">
+      <ReactTyped strings={['A CS student at UVA', 'A Full-Stack Developer', 'An AI Engineer']} typeSpeed={100} backSpeed={100} startDelay={3000} backDelay={1000} cursorChar='' loop/>
+        
       </h3>
       <div></div>
     </section>
---
+++ app/components/partials/dots.tsx
@@ -0,0 +1,8 @@
+export default function dots() {
+
+    
+
+  return (
+    <div className="w-2 h-2 bg-Cmain rounded absolute "></div>
+  )
+}
