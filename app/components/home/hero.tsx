d04b6f93ff9fbd8984dd137e01c5920f237b03f6
MSI Laptop <henrymunprof@gmail.com>
2024-06-25 09:43:04 -0400

    Dots start

--- app/components/home/hero.tsx
+++ app/components/home/hero.tsx
@@ -1,5 +1,4 @@
 'use client'
-import {ReactTyped} from 'react-typed'; 
 import Dots from '../partials/dots';
 
 export default function Hero() {
@@ -9,10 +8,10 @@ export default function Hero() {
     <section id="home" className="items-center text-center bw-text font-playfairDisplay flex flex-col gap-12 min-h-screen justify-center ">
       <Dots></Dots>
       <h2 className="text-8xl font-bold typewriter">
-      <ReactTyped strings={['Hi, I\'m Henry']} typeSpeed={100} cursorChar=''/>
+      Hi, I'm Henry
       </h2>
-      <h3 className="text-4xl min-h-10">
-      <ReactTyped strings={['A CS student at UVA', 'A Full-Stack Developer', 'An AI Engineer']} typeSpeed={100} backSpeed={100} startDelay={3000} backDelay={1000} cursorChar='' loop/>
+      <h3 className="text-4xl min-h-10">A CS student at UVA
+      
         
       </h3>
       <div></div>
--- app/components/partials/dots.tsx
+++ app/components/partials/dots.tsx
@@ -3,6 +3,6 @@ export default function dots() {
     
 
   return (
-    <div className="w-2 h-2 bg-Cmain rounded absolute "></div>
+    <div className="w-2 h-2 bg-Cmain rounded-full absolute "></div>
   )
 }
