20d1a39cfe3db09081414f9a0cc648164496d7d4
HMPrgm - PC <henrymprof@gmail.com>
2024-06-24 22:16:14 -0400

    Updated Contact page sizing

--- app/components/home/contact.tsx
+++ app/components/home/contact.tsx
@@ -10,14 +10,14 @@ export default function Contact() {
     }>({ name: "", email: "", message: "" });
 
     return (
-        <div  id="contact"  className="bg-Cwhite py-10 text-Cblack">
+        <div  id="contact"  className="bg-Cwhite py-20 xl:pb-60 text-Cblack">
             <div className="container mx-auto items-center text-center ">
-                <div className="container items-center mx-auto max-w-96 [&>*]:py-4">
-                    <h2 className="font-playfairDisplay text-5xl  text-Cblack">Let's Talk!</h2>
+                <div className="container items-center mx-auto max-w-96 gap-12 [&>*]:py-4">
+                    <h2 className="font-playfairDisplay text-5xl pb-12 text-Cblack">Let's Talk!</h2>
                     <p className="text-xl">Have a question or just want to talk? Feel free to shoot me a message!</p>
                 </div>
                 <div className="my-10 max-w-[60em] container items-center mx-auto">
-                    <form action="/email" method="POST"className="grid grid-cols-2 gap-4 [&>*]:flex [&>*]:flex-col text-left text-xl">
+                    <form action="/email" method="POST"className="xl:pt-20 grid grid-cols-2 gap-12 [&>*]:flex [&>*]:flex-col text-left text-xl">
                             <div className="">
                                 <label htmlFor="name" className="">Name</label>
                                 <input type="name" name="name" id="name" className="focus:outline-none bg-Cwhite border-b-Cblack border-b-2 py-1" placeholder="Enter your name"></input>
--- app/components/home/hero.tsx
+++ app/components/home/hero.tsx
@@ -9,10 +9,10 @@ export default function Hero() {
     <section id="home" className="items-center text-center bw-text font-playfairDisplay flex flex-col gap-12 min-h-screen justify-center ">
       {/* <Dots></Dots> */}
       <h2 className="text-8xl font-bold typewriter">
-      <ReactTyped strings={['Hi, I\'m Henry']} typeSpeed={100} cursorChar=''/>
+      <ReactTyped strings={['Hi, I\'m Henry']} typeSpeed={80} cursorChar=''/>
       </h2>
       <h3 className="text-4xl min-h-10">
-      <ReactTyped strings={['A CS student at UVA', 'A Full-Stack Developer', 'An AI Engineer']} typeSpeed={100} backSpeed={50} startDelay={3000} backDelay={1000} cursorChar='' loop/>
+      <ReactTyped strings={['A CS student at UVA', 'A Full-Stack Developer', 'An AI Engineer']} typeSpeed={100} backSpeed={50} startDelay={1000} backDelay={1000} cursorChar='' loop/>
         
       </h3>
       <div></div>
