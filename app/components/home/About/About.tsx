12673cfeff45c7c70c304a7d48f57843c7e22895
MSI Laptop <henrymunprof@gmail.com>
2024-06-25 20:30:39 -0400

    Added icon photos

--- app/components/home/About/About.tsx
+++ app/components/home/About/About.tsx
@@ -5,23 +5,23 @@ interface Section{
   title:string;
   text:string;
   left:boolean;
-  photo:string;
+  photos:string[];
 }
 
 export default function About() {
 
-  const sections = [
+  const sections:Section[] = [
         {
             title:"I'm studying Computer Science at the University of Virginia",
             text:"Studying CS through UVA Engineering along with a Math major. Aside from those, my other interests include: history, language learning (German currently), reading, and exercising!",
             left: false,
-            photo:'Photo_Cropped.png'
+            photos:['Photo_Cropped.png']
         },
         {
             title:"I'm a Full-Stack Web Developer",
             text:"Working in teams or alone, I'm passionate about all things web. The technology I'm most interested in at the moment is Next.js. The combination of creativity and problem solving drives my excitement for web dev.",
             left:true,
-            photo:''
+            photos:['./technology_photos/Angular.png','./technology_photos/React.png','./technology_photos/Nextjs.png','./technology_photos/Git.png','./technology_photos/Htmlcss.png','./technology_photos/Tailwind.png','./technology_photos/Javascript.png','./technology_photos/Typescript.png','./technology_photos/Expressjs.png','./technology_photos/Nodejs.png','./technology_photos/Mongodb.png']
         }
     ]
 
@@ -32,7 +32,7 @@ export default function About() {
           <h2 className="font-playfairDisplay text-5xl pb-10">Who Am I?</h2>
         </Reveal>
 
-        {sections.map(s=> <About_section title={s.title} text={s.text} left={s.left} photo={s.photo}></About_section>)}
+        {sections.map(s=> <About_section title={s.title} text={s.text} left={s.left} photos={s.photos}></About_section>)}
     </section>
   )
 }
--- app/components/home/About/About_section.tsx
+++ app/components/home/About/About_section.tsx
@@ -1,6 +1,6 @@
 import Reveal from "../../partials/effects/revealonscroll";
 
-export default function About_section({title, text, left, photo}: {title:string, text:string, left:boolean, photo:string}) {
+export default function About_section({title, text, left, photos}: {title:string, text:string, left:boolean, photos:string[]}) {
 
   // const title:string = "I’m studying Computer Science at the University of Virginia";
   // const text:string = "Studying CS through UVA Engineering along with a Math major. Aside from those, my other interests include: history, language learning (German currently), reading, and exercising!"
@@ -10,7 +10,11 @@ export default function About_section({title, text, left, photo}: {title:string,
     <div className="py-10 container mx-auto lg:w-[70rem] sm:w-[50rem] flex flex-col gap-16">
       <div className="grid grid-cols-2 gap-10 items-center">
           <Reveal translate="-translate-x-20"className={`${left? "order-last":""} flex justify-center`}>
-            <img src={photo} alt={title} className="max-h-80 rounded-t-full bg-Cwhite " />
+            {photos.length <= 1?
+            <img src={photos[0]} alt={title} className="max-h-80 rounded-t-full bg-Cwhite " />
+            :
+            <></>
+            }
         </Reveal>
         <Reveal translate="translate-x-20">
           <div className="flex flex-col gap-2 text-left">
