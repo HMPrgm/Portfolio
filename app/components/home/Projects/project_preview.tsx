075e7141c34ea362d75084d49dcb4c8df35d640b
MSI Laptop <henrymunprof@gmail.com>
2024-06-26 19:16:23 -0400

    No-Title transition

--- app/components/home/Projects/project_preview.tsx
+++ app/components/home/Projects/project_preview.tsx
@@ -7,7 +7,7 @@ export default function ProjectPreview({ photo='', project }: { photo?:string, p
   return (
     <div className="grid  p-12 bg-Cdarkmain text-Cwhite container items-center mx-auto lg:w-[50rem] my-10 hover:lg:w-[55rem] hover:py-16 hover:bg-Cmain transition-all duration-500 ease-out group">
       <div className="text-center flex flex-col gap-3 items-center justify-center">
-        <h3 className="text-3xl group-hover:text-4xl transition-all duration-500 ease-out font-playfairDisplay">{project.title}</h3>
+        <h3 className="text-3xl  transition-all duration-500 ease-out font-playfairDisplay">{project.title}</h3>
         <p className="pb-2">{project.desc}</p>
         <button className="">
           <Link href={project.link} target="_blank" className="text-center hover:bg-Cwhite hover:text-Cblack transition-all duration-500 ease-out py-3 px-6 max-w-40 border-solid border-2 border-Cwhite  ">More Details</Link>
