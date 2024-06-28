c07108ad395cf20294d6516cdfa21d5dc908c3a2
MSI Laptop <henrymunprof@gmail.com>
2024-06-28 09:47:39 -0400

    Added Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 import type { Metadata } from "next";
 import "./globals.css";
 //Fonts
@@ -33,6 +34,7 @@ export default function RootLayout({
         
         {children}
         <SpeedInsights/>
+        <Analytics />
         <Footer/>
       </body>
     </html>
