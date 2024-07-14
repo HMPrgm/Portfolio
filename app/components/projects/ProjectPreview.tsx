25817956d530936c78a4cf556fe9a3ae2e9e73fc
MSI Laptop <henrymunprof@gmail.com>
2024-07-14 10:48:57 -0400

    updated Icon and fixed link padding in projects page

--- app/components/projects/ProjectPreview.tsx
+++ app/components/projects/ProjectPreview.tsx
@@ -9,9 +9,7 @@ export default function ProjectPreview({project}:{project:Project}) {
       <div className="text-left flex flex-col gap-3">
         <h3 className="text-3xl font-playfairDisplay">{project.title}</h3>
         <p>{project.desc}</p>
-        <button className="border-solid border-2 border-Cwhite py-2 px-4 text-center max-w-40 hover:bg-Cwhite hover:text-Cblack transition duration-500 ease-out">
-          <Link href={project.link} target="_blank">More Details</Link>
-        </button>
+          <Link href={project.link} target="_blank" className="border-solid border-2 border-Cwhite text-center max-w-40 hover:bg-Cwhite hover:text-Cblack transition duration-500 ease-out px-6 py-3">More Details</Link>
       </div>
     </div>
   )
